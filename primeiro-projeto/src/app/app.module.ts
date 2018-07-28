fea988e51b6ebd91ceaf7a63ffac52999d118c06
Lucas Souza <alfsouza708@gmail.com>
2018-07-27 22:18:57 -0300

    Feito primeiro component: primeiro-component

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponent
   ],
   imports: [
     BrowserModule
---
+++ primeiro-projeto/src/app/primeiro/primeiro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'primeiro-component',
+    template: `
+    <p>Meu primeiro component com Angular 6!</p>
+    `
+})
+export class PrimeiroComponent {}
+
+
+
+
