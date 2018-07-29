dd7ef4d88c254884957c760426d217bbbde5815f
Lucas Souza <alfsouza708@gmail.com>
2018-07-28 23:00:21 -0300

    Aula de Template

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal : string;
+
+  jogos : string[] = ['Gold', 'Silver', 'Crystal'];
+
+  constructor() {
+    this.nomePortal = 'http://serebii.net';
+    for (let i=0; i<this.jogos.length;i++){
+      let jogo = this.jogos[i];
+    }
+  }
 
   ngOnInit() {
   }
