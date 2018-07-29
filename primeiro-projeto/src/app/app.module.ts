be72218f5ad6dc1dcaede0d56b404f4cff5543c5
Lucas Souza <alfsouza708@gmail.com>
2018-07-28 22:18:36 -0300

    Treinamento com modules

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { PrimeiroComponent } from './primeiro/primeiro.component';
     PrimeiroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
